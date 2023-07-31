83fa176f42b343e9194fafc1004175f39292454d
Özcan Kara <ozcankara810@gmail.com>
2023-07-31 23:13:12 +0300

    some changes

---
+++ DataAccessLayer/Context.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server= DESKTOP - NVPFKAI\\SQLEXPRESS;database=CoreProjeDB;integrated security=true");
+        }
+
+        public int MyProperty { get; set; }
+    }
+}
+
+//Context class : 1- DB yapilandirma baglanti stringi  2-DB yansitilacak olan Tablolari tutan class
+                  
+                   
