79eeb1c929ec8629918ec3b2bf18ba59b12d8567
Özcan Kara <ozcankara810@gmail.com>
2023-07-31 23:53:48 +0300

    some changes

--- DataAccessLayer/Context.cs
+++ DataAccessLayer/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,21 @@ namespace DataAccessLayer
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("server= DESKTOP - NVPFKAI\\SQLEXPRESS;database=CoreProjeDB;integrated security=true");
+            optionsBuilder.UseSqlServer("server=DESKTOP-NVPFKAI\\SQLEXPRESS;database=CoreProjeDB;integrated security=true");
         }
 
-        public int MyProperty { get; set; }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Experience> Experiences { get; set; }
+        public DbSet<Feature> Features { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Portfolio> Portfolios { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<Skill> Skills { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
 
-//Context class : 1- DB yapilandirma baglanti stringi  2-DB yansitilacak olan Tablolari tutan class
-                  
-                   
+
+
+
