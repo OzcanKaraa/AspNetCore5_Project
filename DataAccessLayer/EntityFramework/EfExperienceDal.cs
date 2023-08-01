8665bc9a94c3a8ff9c55312af41f91eda5a305e9
Özcan Kara <ozcankara810@gmail.com>
2023-08-01 18:31:05 +0300

    some changes

---
+++ DataAccessLayer/EntityFramework/EfExperienceDal.cs
@@ -0,0 +1,40 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfExperienceDal : GenericRepository<Exception>, IExperienceDal
+    {
+        public void Delete(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Experience t)
+        {
+            throw new NotImplementedException();
+        }
+
+        Experience IGenericDal<Experience>.GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        List<Experience> IGenericDal<Experience>.GetList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
