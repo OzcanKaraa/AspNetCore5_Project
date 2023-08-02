9b487379293a5e342df11476b3a3cd4356de1e01
Özcan Kara <ozcankara810@gmail.com>
2023-08-02 13:41:06 +0300

    some changes

---
+++ BusinessLayer/Concrete/AbaoutManager.cs
@@ -0,0 +1,38 @@
+﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    internal class AbaoutManager : IAboutService
+    {
+        public About GeyByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TAdd(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(About t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<About> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(About t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
