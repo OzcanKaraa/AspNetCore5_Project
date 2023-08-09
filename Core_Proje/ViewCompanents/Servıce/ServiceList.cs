813dd8170590196b24a48beb03b3d3a5b9d6f31a
Özcan Kara <ozcankara810@gmail.com>
2023-08-10 00:11:08 +0300

    some changes

---
+++ Core_Proje/ViewCompanents/Servıce/ServiceList.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewCompanents.Servıce
+{
+    public class ServiceList : ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IViewComponentResult Invoke()
+        {
+            var values = featureManager.TGetList();
+            return View(values);
+        }
+    {
+    }
+}
