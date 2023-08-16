3259385bcf0c75c3c451f7f17ad0055a2e8f129c
Özcan Kara <ozcankara810@gmail.com>
2023-08-16 06:10:24 +0300

    some changes

---
+++ Core_Proje/ViewCompanents/Feature/FeatureList.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Proje.ViewCompanents.Feature
+{
+    public class FeatureList: ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
